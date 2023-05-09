99847d6f8b9ff37f45181ac057dfcb1b5db29ae5
fakih-ibrohim <fakihibrohin37@gmail.com>
2023-05-09 17:23:30 +0700

    IT-Asset

---
+++ src/main/java/assetIT/ITassetApplication.java
@@ -0,0 +1,13 @@
+package assetIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ITassetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ITassetApplication.class, args);
+	}
+
+}
